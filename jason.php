0027ea9cc1666710ab71c69b1843aa9d847e5a20
lesovics89 <58247340+lesovics89@users.noreply.github.com>
2019-12-23 19:05:57 +0100

    Add files via upload

--- jason.php
+++ jason.php
@@ -19,21 +19,35 @@ body {
 -->
 </style>
 <?php
-    
-	$jsonData=file_get_contents("https://eune.leagueoflegends.com/hu/rss.xml");
+    header ('Content-type: text/html; charset=utf-8');
+	$jsonData=file_get_contents("https://feed2json.org/convert?url=https%3A%2F%2Feune.leagueoflegends.com%2Fhu%2Frss.xml&minify=on");
+	
 	$json=json_decode($jsonData,true);
+	
     $m="<br>";
 	
     $l="<p>&nbsp;</p>";
 	$k="&nbsp;";
-	
-	
-	
-	echo $json['title'][0];
-	
+	echo $json['data']['Aatrox']['name'],$m;
+	echo $json['data']['Aatrox']['blurb'];
+	echo $json['data']['Aatrox']['image']['full'];
+		
 ?>
-	
-	
+	<?php
+$rss_feed = simplexml_load_file("rss.json");
+if(!empty($rss_feed)) {
+$i=0;
+foreach ($rss_feed->channel->item as $feed_item) {
+if($i>=10) break;
+?>
+<div><a class="feed_title" href="<?php echo $feed_item->link; ?>"><?php echo $feed_item->title; ?></a></div>
+<div><?php echo implode(' ', array_slice(explode(' ', $feed_item->description), 0, 14)) . "..."; ?></div>
+<?php		
+$i++;	
+}}
+?>
+
+
 	
 	
 	
