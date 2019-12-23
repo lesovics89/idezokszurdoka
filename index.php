b6f51b0dae11c9a23096d9fca04ab755db1fe387
lesovics89 <58247340+lesovics89@users.noreply.github.com>
2019-12-23 11:36:39 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,8 @@
-
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-  <link rel="stylesheet" type="text/css" href="assets/main.css">
-<meta name="description" content="Lesovics Péter névjegy">
-  <style>
-@import url('https://fonts.googleapis.com/css?family=Dancing+Script&display=swap');
-</style>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>Lesovics Péter web programozó</title>
-</head>
-<body>
-  
- <div id="profile-card">
-   <div id="head">
-     <img id="profile-image" src="https://i.imgur.com/ZT9lARw.jpg" alt="profil">
-   
-     <div id="name-headline">
-       <strong></strong>
-       <h1>Üdvözöllek az idézők szurdokában</h1>
-	   
-	   <iframe src="https://lesovics89.github.io/idezokszurdoka/hir.php"></iframe>
-       
-     </div>
-     
-     </div>
-     <div id="content">
-    
-       <p>Elérhetőségeim </p>
-       
-   </div>
-     <a class="fa fa-twitter" href="https://twitter.com/"> Twitter </a>
-     <a class="fa fa-facebook" href="https://www.facebook.com/peter.lesovics"> Facebook </a>
-     <a class="fa fa-instagram" href="https://instagram.com/"> Instagram </a>
-     
- </div>
-   
+<?php
  
-</body>
-</html>
+   
+   $url = "https://eune.leagueoflegends.com/hu/rss.xml";
+$xml = simplexml_load_file($url);
+print_r($xml);
+   
+   ?>
