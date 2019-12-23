6e239fbaf9db8190e6728b67d0d4c44cdc2f34c4
lesovics89 <58247340+lesovics89@users.noreply.github.com>
2019-12-23 09:06:09 +0100

    Add files via upload

--- hir.php
+++ hir.php
@@ -1,6 +1,17 @@
 
+
+
+<!DOCTYPE html>
+
+<head>
+ 
+</head>
+<body>
 <?php
+$url = simplexml_load_file("https://eune.leagueoflegends.com/hu/rss.xml");
 
-
-echo "naaaaaaacfcfgcfgcfgcfgcgfhchgfcfghcgfhchgfchgfa";
+echo $url->channel[0];
 ?>
+ 
+</body>
+</html>
